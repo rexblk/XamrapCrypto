5b609586d59b244ae7c30957efaeb1b559868068
rexblk <rexnblack@gmail.com>
2016-03-06 14:35:36 -0600

    Xamrap v1.0
    Added all project filed to repository

---
+++ App1Tests/MainActivityTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xamrap_Crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamrap_Crypto.Tests
+{
+    [TestClass()]
+    public class MainActivityTests
+    {
+        [TestMethod()]
+        public void ComplexSubCypherEncodeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
